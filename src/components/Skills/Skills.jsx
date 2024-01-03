1e50a4c0bfddc2ac9dee85e69455d4695f3f500e
Kateryna <k.borysenko.kyiv@gmail.com>
2024-01-03 16:04:45 +0200

    added filter

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,19 +1,60 @@
+import React, { useState, useEffect } from 'react';
 import Paper from '../common/Paper/Paper';
 import { skills } from '../../data/skills';
 import s from './Skills.module.css';
 
 const Skills = () => {
+  const [filteredSkills, setFilteredSkills] = useState(skills);
+  const [activeButton, setActiveButton] = useState('all');
+
+  useEffect(() => {
+    if (activeButton === 'all') {
+      setFilteredSkills(
+        [...skills].sort((a, b) => a.name.localeCompare(b.name)),
+      );
+    } else {
+      setFilteredSkills(skills.filter(skill => skill.type === activeButton));
+    }
+  }, [activeButton]);
+
+  const skillTypes = skills.reduce((unique, skill) => {
+    if (!unique.includes(skill.type)) {
+      unique.push(skill.type);
+    }
+    return unique;
+  }, []);
+
   return (
     <Paper>
-      <div className={s.list}>
-        {skills.map(({ id, name, path, type, alt }) => (
-          <div key={id} className={s.item}>
-            <div className={s.img_container}>
-              <img src={path} alt={alt} />
+      <div className={s.container}>
+        <div className={s.btn_container}>
+          <button
+            className={activeButton === 'all' ? s.activeButton : ''}
+            onClick={() => setActiveButton('all')}
+          >
+            All
+          </button>
+
+          {skillTypes.map(type => (
+            <button
+              key={type}
+              className={activeButton === type ? s.activeButton : ''}
+              onClick={() => setActiveButton(type)}
+            >
+              {type}
+            </button>
+          ))}
+        </div>
+        <div className={s.list}>
+          {filteredSkills.map(({ id, name, path, type, alt }) => (
+            <div key={id} className={s.item}>
+              <div className={s.img_container}>
+                <img src={path} alt={alt} />
+              </div>
+              <div className={s.title}>{name}</div>
             </div>
-            <div className={s.title}>{name}</div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </Paper>
   );
