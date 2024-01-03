8ecdce1bad6cdd045306fffa6319f4e8ee3a2308
Kateryna <k.borysenko.kyiv@gmail.com>
2024-01-03 14:03:12 +0200

    added data of skills

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,7 @@
+import s from './Skills.module.css';
+
+const Skills = () => {
+  return <div className={s.container}>Skills</div>;
+};
+
+export default Skills;
---
+++ src/data/skills.js
@@ -0,0 +1,164 @@
+export const skills = [
+  {
+    id: 1,
+    name: 'Agile',
+    path: '../images/skills/agile.png',
+    type: 'Development Tools'
+  },
+  {
+    id: 2,
+    name: 'Apollo',
+    path: '../images/skills/apollo.png',
+    type: 'Frameworks and Libraries'
+  },
+  {
+    id: 3,
+    name: 'CSS3',
+    path: '../images/skills/css3.png',
+    type: 'Programming Languages'
+  },
+  {
+    id: 4,
+    name: 'Express',
+    path: '../images/skills/express.png',
+    type: 'Frameworks and Libraries'
+  },
+  {
+    id: 5,
+    name: 'Figma',
+    path: '../images/skills/figma.png',
+    type: 'Testing and Design'
+  },
+  {
+    id: 6,
+    name: 'Firebase',
+    path: '../images/skills/firebase.png',
+    type: 'Databases and APIs'
+  },
+  {
+    id: 7,
+    name: 'Formik',
+    path: '../images/skills/formik.png',
+    type: 'Frameworks and Libraries'
+  },
+  {
+    id: 8,
+    name: 'Git',
+    path: '../images/skills/git.png',
+    type: 'Development Tools'
+  },
+  {
+    id: 9,
+    name: 'GitHub',
+    path: '../images/skills/github.png',
+    type: 'Development Tools'
+  },
+  {
+    id: 10,
+    name: 'GitLab',
+    path: '../images/skills/gitlab.png',
+    type: 'Development Tools'
+  },
+  {
+    id: 11,
+    name: 'GraphQL',
+    path: '../images/skills/graphql.png',
+    type: 'Databases and APIs'
+  },
+  {
+    id: 12,
+    name: 'HTML5',
+    path: '../images/skills/html5.png',
+    type: 'Programming Languages'
+  },
+  {
+    id: 13,
+    name: 'Jest',
+    path: '../images/skills/jest.png',
+    type: 'Testing and Design'
+  },
+  {
+    id: 14,
+    name: 'Jira',
+    path: '../images/skills/jira.png',
+    type: 'Development Tools'
+  },
+  {
+    id: 15,
+    name: 'JS',
+    path: '../images/skills/js.png',
+    type: 'Programming Languages'
+  },
+  {
+    id: 16,
+    name: 'Next JS',
+    path: '../images/skills/next-js.png',
+    type: 'Frameworks and Libraries'
+  },
+  {
+    id: 17,
+    name: 'Node JS + Express',
+    path: '../images/skills/node-js-express.jpeg',
+    type: 'Frameworks and Libraries'
+  },
+  {
+    id: 18,
+    name: 'React Hook Form',
+    path: '../images/skills/react-hook-form.png',
+    type: 'Frameworks and Libraries'
+  },
+  {
+    id: 19,
+    name: 'React Native',
+    path: '../images/skills/react-native.png',
+    type: 'Frameworks and Libraries'
+  },
+  {
+    id: 20,
+    name: 'React Testing Library',
+    path: '../images/skills/react-testing-library.png',
+    type: 'Testing and Design'
+  },
+  {
+    id: 21,
+    name: 'React',
+    path: '../images/skills/react.png',
+    type: 'Frameworks and Libraries'
+  },
+  {
+    id: 22,
+    name: 'Redux',
+    path: '../images/skills/redux.png',
+    type: 'Frameworks and Libraries'
+  },
+  {
+    id: 23,
+    name: 'REST API',
+    path: '../images/skills/rest-api.png',
+    type: 'Databases and APIs'
+  },
+  {
+    id: 24,
+    name: 'SASS',
+    path: '../images/skills/sass.png',
+    type: 'Testing and Design'
+  },
+  {
+    id: 25,
+    name: 'Styled components',
+    path: '../images/skills/styled-components.png',
+    type: 'Testing and Design'
+  },
+  {
+    id: 26,
+    name: 'Trello',
+    path: '../images/skills/trello.png',
+    type: 'Development Tools'
+  },
+  {
+    id: 27,
+    name: 'Mongo DB',
+    path: '../images/skills/mongodb.png',
+    type: 'Databases and APIs'
+  },
+]
