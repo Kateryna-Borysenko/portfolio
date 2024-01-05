ee74b57c5cfa1994cdcd33492a6e9d305cbdde8c
Kateryna <k.borysenko.kyiv@gmail.com>
2024-01-05 13:47:56 +0200

    added mock data to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,21 +1,51 @@
 import { motion } from 'framer-motion';
+import univer from '../../images/projects/univer.png';
+import portfolio from '../../images/projects/portfolio.png';
+import todo from '../../images/projects/todo.png';
+import filmoteka from '../../images/projects/univer.png';
 import Paper from '../../components/common/Paper/Paper';
 import s from './Projects.module.css';
 
 const Projects = () => {
-  const sliderVariants = {
-    initial: {
-      x: 0,
+  const items = [
+    {
+      id: 1,
+      title: 'Univer React App',
+      img: univer,
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
     },
-    animate: {
-      x: '-165%',
-      transition: {
-        repeat: Infinity,
-        repeatType: 'mirror',
-        duration: 20,
-      },
+    {
+      id: 2,
+      title: 'Portfolio React App',
+      img: portfolio,
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
     },
-  };
+    {
+      id: 3,
+      title: 'Todo React App',
+      img: todo,
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
+    },
+    {
+      id: 4,
+      title: 'Vanilla JS App',
+      img: filmoteka,
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.',
+    },
+  ];
+  // const sliderVariants = {
+  //   initial: {
+  //     x: 0,
+  //   },
+  //   animate: {
+  //     x: '-165%',
+  //     transition: {
+  //       repeat: Infinity,
+  //       repeatType: 'mirror',
+  //       duration: 20,
+  //     },
+  //   },
+  // };
 
   return (
     <div className={s.container}>
