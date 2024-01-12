f3f07cd5b433ebb4fad92eb722933099884f60e9
Kateryna <k.borysenko.kyiv@gmail.com>
2024-01-12 16:01:59 +0200

    all screens of forms is completed

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/common/Header/Header';
 import Footer from '../../components/common/Footer/Footer';
-import s from './Layout.module.css';
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <main className={s.main_container}>
+      <main>
         <Outlet />
       </main>
       <Footer />
