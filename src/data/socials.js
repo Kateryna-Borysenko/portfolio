cbaa17933f5aa5183b0595cf9790ab70d32c7152
Kateryna <k.borysenko.kyiv@gmail.com>
2024-05-09 14:08:31 +0300

    changed sosial icons

--- src/data/socials.js
+++ src/data/socials.js
@@ -1,28 +1,31 @@
-import facebookIcon from '../images/socials/facebook.png'
-import githubIcon from '../images/socials/github.png'
-import instagramIcon from '../images/socials/instagram.png'
-import linkedinIcon from '../images/socials/linkedin.png'
-import { GITHUB_URL, LINKEDIN_URL, INSTAGRAM_URL, FACEBOOK_URL } from '../utils/urls'
+import facebookIcon from '../images/socials/facebook.svg';
+import githubIcon from '../images/socials/github.svg';
+import instagramIcon from '../images/socials/instagram.svg';
+import linkedinIcon from '../images/socials/linkedin.svg';
+import {
+  GITHUB_URL,
+  LINKEDIN_URL,
+  INSTAGRAM_URL,
+  FACEBOOK_URL,
+} from '../utils/urls';
 
-export const socials = [{
-  id: 1,
-  name: 'GIT HUB',
-  icon: githubIcon,
-  url: GITHUB_URL
-}, {
-  id: 2,
-  name: 'LINKED IN',
-  icon: linkedinIcon,
-  url: LINKEDIN_URL
-}, {
-  id: 3,
-  name: 'INSTAGRAM',
-  icon: instagramIcon,
-  url: INSTAGRAM_URL
-}, {
-  id: 4,
-  name: 'FACEBOOK',
-  icon: facebookIcon,
-  url: FACEBOOK_URL
-},
-]
+export const socials = [
+  {
+    id: 1,
+    name: 'LINKED IN',
+    icon: linkedinIcon,
+    url: LINKEDIN_URL,
+  },
+  {
+    id: 2,
+    name: 'INSTAGRAM',
+    icon: instagramIcon,
+    url: INSTAGRAM_URL,
+  },
+  {
+    id: 3,
+    name: 'FACEBOOK',
+    icon: facebookIcon,
+    url: FACEBOOK_URL,
+  },
+];
