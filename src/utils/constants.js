cccf07a58b04baf7b80580f5ea2f17bbd69bfa8a
Kateryna <k.borysenko.kyiv@gmail.com>
2024-06-09 20:19:27 -0400

    add updated resume

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const PHONE_NUMBER = '+1 (000) 000-0000';
+export const PHONE_NUMBER = '+1 (727) 598-7543';
 export const EMAIL = 'borysenko.info@gmail.com';
 export const LOCATION = 'Florida, USA';
